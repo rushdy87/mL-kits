83a54804ad692b3e0a750aee09d1b2c79098ea62
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-01-05 22:58:09 +0300

    implementing k-nearest neighbors algorithm

--- plinko/score.js
+++ plinko/score.js
@@ -1,4 +1,6 @@
 const outputs = [];
+const predicationPoint = 300;
+const k = 4;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
@@ -7,5 +9,23 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
   // Write code here to analyze stuff
-  console.log(outputs);
+  const bucket = _.chain(outputs)
+    .map((row) => [distance(row[0]), row[3]])
+    .sortBy((row) => row[0])
+    .slice(0, k)
+    .countBy((row) => row[1])
+    .toPairs()
+    .sortBy((row) => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+
+  console.log(
+    `Wheh you drop the from ${predicationPoint} it most likly will fall into the bucket number ${bucket}.`
+  );
+}
+
+function distance(point) {
+  return Math.abs(point - predicationPoint);
 }
