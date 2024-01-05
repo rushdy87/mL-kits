ccd394d289569bd0a6119c547245c621595e4ff3
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-01-05 18:57:08 +0300

    creating a dataset

---
+++ notes.md
@@ -0,0 +1,2 @@
+<h2>Goal</h2>
+<p>Given some data about where a ball is dropped from, can we predict what bucket it will end up in?</p>
--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,11 @@
+const outputs = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
+  console.log(outputs);
 }
-
