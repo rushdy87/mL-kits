133c764fef8b68eb511bc00fc4f6fab132efb89b
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-01-12 01:43:41 +0300

    Running an analysis

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -3,6 +3,22 @@ const tf = require('@tensorflow/tfjs');
 
 const loadCSV = require('./load-csv');
 
+function knn(features, labels, productionPoint, k) {
+  return (
+    features
+      .sub(productionPoint)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => (a.arraySync()[0] > b.arraySync()[0] ? 1 : -1))
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.arraySync()[1], 0) / k
+  );
+}
+
 let { features, labels, testFeatures, testLabels } = loadCSV(
   'kc_house_data.csv',
   {
@@ -13,5 +29,9 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   }
 );
 
-console.log(testFeatures);
-console.log(testLabels);
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+const result = knn(features, labels, tf.tensor(testFeatures[0]), 10);
+
+console.log(`Guess is: ${result}, and an actual value is: ${testLabels[0]} `);
